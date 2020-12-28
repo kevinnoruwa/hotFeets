6bd9e4a16cb973e9966edb66e982780a52abf3d3
The-Unknown-Developer <theunknowndevloper@gmail.com>
2020-12-27 20:25:14 -0500

    deploy

--- config/database.php
+++ config/database.php
@@ -46,10 +46,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'us-cdbr-east-02.cleardb.com'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'heroku_cbbe7f6a4f0afd9'),
-            'username' => env('DB_USERNAME', 'b0c65138200b35'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
             'unix_socket'   => getenv('UNIX_SOCKET'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
